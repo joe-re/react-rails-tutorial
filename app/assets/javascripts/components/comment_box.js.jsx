7bebfe8233b8b43eaa42ff7b9f58f73ac520d117
joe-re <joe.tialtngo@gmail.com>
2015-04-28 02:53:57 +0900

    Using Props

--- app/assets/javascripts/components/comment_box.js.jsx
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -29,3 +29,16 @@ var CommentForm = React.createClass({
     );
   }
 });
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
