ea30e2fd93f0fad331f9867dee5ec439d5ece390
joe-re <joe.tialtngo@gmail.com>
2015-04-28 02:54:10 +0900

    Component Properties

--- app/assets/javascripts/components/comment_box.js.jsx
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -14,7 +14,8 @@ var CommentList = React.createClass({
   render: function() {
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
       </div>
     );
   }
