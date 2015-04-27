ddd839ecff5ea715190481dc32e43b887f25321e
joe-re <joe.tialtngo@gmail.com>
2015-04-28 01:57:24 +0900

    setting for react

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,12 @@ module ReactRailsTutorial
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # for react-rails
+    config.react.addons = true
+    config.react.jsx_transform_options = {
+      harmony: true,
+      # strip_types: true, # for removing Flow type annotations
+    }
   end
 end
