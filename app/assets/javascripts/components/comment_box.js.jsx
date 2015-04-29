c97293da7ac7f8f54256f9d2b51564ad9d474f58
joe-re <joe.tialtngo@gmail.com>
2015-04-29 15:14:31 +0900

    Reactive state

--- app/assets/javascripts/components/comment_box.js.jsx
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -1,9 +1,12 @@
 var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
