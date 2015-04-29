3221bb558a124407c8d069977d991750e9216d8a
joe-re <joe.tialtngo@gmail.com>
2015-04-29 17:53:59 +0900

    Optimization: optimistic updates

--- app/assets/javascripts/components/comment_box.js.jsx
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -12,13 +12,13 @@ var CommentBox = React.createClass({
     });
   },
   handleCommentSubmit: function(comment) {
+    this.setState({data: this.state.data.concat([comment])});
     $.ajax({
       url: this.props.url,
       dataType: 'json',
       type: 'POST',
       data: comment,
       success: function(data) {
-        this.setState({data: this.state.data.concat([data])});
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(this.props.url, status, err.toString());
