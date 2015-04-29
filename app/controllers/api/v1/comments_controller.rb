e8ff6cdaf5cee81846c648cd1d8409f9cd52bea6
joe-re <joe.tialtngo@gmail.com>
2015-04-29 16:38:02 +0900

    サーバを実装する

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,8 +1,16 @@
 class Api::V1::CommentsController < ApplicationController
   def index
-    @data = [
-      { author: 'Pete Hunt', text: 'This is one comment' },
-      { author: 'Jordan Walke', text: 'This is *another* comment' }
-    ]
+    @data = Comment.all
+  end
+
+  def create
+    Comment.create(comment_params)
+    render nothing: true, status: :created
+  end
+
+  private
+
+  def comment_params
+    params.permit(:author, :text)
   end
 end
